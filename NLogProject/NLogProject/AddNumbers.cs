43b148fe6e74cda6b5dda08513f368c6ed9905ba
Manoj Kumar Manukonda <manojkumarmanukonda77@gmail.com>
2022-11-08 09:29:49 +0530

    [Manoj] Add: NLog

---
+++ NLogProject/NLogProject/AddNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogProject
+{
+    public class AddNumbers
+    {
+        private readonly NLog nLog = new NLog();
+        public int Sum(int firstNumber, int secondNumber)
+        {
+            if(firstNumber == 0 || secondNumber == 0)
+            {
+                nLog.LogDebug("Debug Successful : Sum()");
+                nLog.LogError("Expecting null values");
+                nLog.LogWarn("firstNumber or secondNumber should not equal to zero");
+            }
+            int result = secondNumber + firstNumber;
+            nLog.LogDebug("Debug Successful : Sum()");
+            nLog.LogInfo("Sum Method Passed, Result" + result);
+            return result;
+        }
+    }
+}
---
+++ NLogProject/NLogProject/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace NLogProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Addition Problem");
+            AddNumbers addNumbers = new AddNumbers();
+            Console.WriteLine("Result is " + addNumbers.Sum(0, 1));
+        }
+    }
+}
